dd8204fc720d44dc1e248b060677b41a6afe6428
Xiaoni-61 <59406701+Xiaoni-61@users.noreply.github.com>
2022-01-24 12:22:51 +0800

    quicksort

---
+++ AcWing/basic algorithm/quicksort.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+
+const int N = 1e6 + 10;
+int n;
+int q[N];
+void quicksort(int q[], int low, int high)
+{
+    if (low >= high)
+        return;
+    
+    int temp = q[low];
+    int i=low;
+    int j=high;
+    
+    
+    while(i<j){
+        while(i<j&&q[j]>=temp)j--;
+        q[i]=q[j];
+        while(i<j&&q[i]<=temp)i++;
+        q[j]=q[i];
+    }
+    q[i]=temp;
+    quicksort(q,low,i-1);
+    quicksort(q,i+1,high);
+}
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &q[i]);
+    }
+    //quicksort(q, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", q[i]);
+    }
+    printf("哈哈哈\n");
+}
